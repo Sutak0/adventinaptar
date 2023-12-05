d02c126db6d2f4ee72368b21e6948d70d0dee1f1
Sutak0 <70635825+Sutak0@users.noreply.github.com>
2023-12-05 09:53:26 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+
+function openMyFckingWindow(win){
+    d = new Date();
+    num = d.getDate();
+
+    selectedNum = win.innerText * 1
+
+    if (selectedNum === num || selectedNum < num) {
+        alert("nyissad mar latni akarom en is😁😁😁😁😁😁😁😁😁😁")
+    } else {
+        alert("ezt majd kesobb ocsipok 😬😬👎🏿👎🏿🗣️🗣️")
+    }
+}
